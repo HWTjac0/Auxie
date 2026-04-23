6312aa868805dd9126614d40d61354c7806db213
jacmill <jakub3020.jmy@gmail.com>
2026-04-23 19:49:32 +0200

    added models based on schema

---
+++ backend/internal/models/models.go
@@ -0,0 +1,66 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+type User struct {
+	ID            int            `db:"id"`
+	Email         string         `db:"email"`    // NOT NULL
+	Username      string         `db:"username"` // NOT NULL
+	Type          sql.NullString `db:"type"`
+	SpotifyID     sql.NullString `db:"spotify_id"`
+	SoundCloudID  sql.NullString `db:"soundcloud_id"`
+	TidalID       sql.NullString `db:"tidal_id"`
+	SpotifyKey    sql.NullString `db:"spotify_key"`
+	SoundCloudKey sql.NullString `db:"soundcloud_key"`
+	TidalKey      sql.NullString `db:"tidal_key"`
+	CurrentRoomID sql.NullInt64  `db:"current_room_id"`
+	CurrentRole   sql.NullString `db:"current_role"`
+	CreatedAt     time.Time      `db:"created_at"`
+}
+
+type Room struct {
+	ID                 int           `db:"id"`
+	Name               string        `db:"name"`    // NOT NULL
+	HostID             int           `db:"host_id"` // NOT NULL
+	LastPlayedPosition sql.NullInt64 `db:"last_played_position"`
+	CreatedAt          time.Time     `db:"created_at"`
+}
+
+type Track struct {
+	ID        int            `db:"id"`
+	SourceURI string         `db:"source_uri"` // NOT NULL
+	Artist    sql.NullString `db:"artist"`
+	Title     string         `db:"title"` // NOT NULL
+	Album     sql.NullString `db:"album"`
+	CoverURL  sql.NullString `db:"cover_url"`
+	Platform  sql.NullString `db:"platform"`
+}
+
+type RoomTrack struct {
+	ID             int          `db:"id"`
+	RoomID         int          `db:"room_id"`  // NOT NULL
+	TrackID        int          `db:"track_id"` // NOT NULL
+	AddedBy        int          `db:"added_by"` // NOT NULL
+	Position       int          `db:"position"` // NOT NULL
+	Status         string       `db:"status"`   // NOT NULL
+	StartTimestamp sql.NullTime `db:"start_timestamp"`
+	EndTimestamp   sql.NullTime `db:"end_timestamp"`
+	LikeCount      int          `db:"like_count"`
+	SkipCount      int          `db:"skip_count"`
+}
+
+type Archival struct {
+	ID        int           `db:"id"`
+	RoomID    sql.NullInt64 `db:"room_id"`
+	Name      string        `db:"name"` // NOT NULL
+	CreatedAt time.Time     `db:"created_at"`
+}
+
+type ArchivalTrack struct {
+	ArchivalID int `db:"archival_id"`
+	TrackID    int `db:"track_id"`
+}
+
