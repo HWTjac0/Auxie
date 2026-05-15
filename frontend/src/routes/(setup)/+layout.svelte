b96f773042654a16df70c21932efc12af1c266d5
jacmill <jakub3020.jmy@gmail.com>
2026-05-15 20:49:38 +0200

    added some resetting styles

---
+++ frontend/src/routes/(setup)/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+let { children } = $props();
+</script>
+
+<div class="wrapper">
+  <div class="setup_container">
+    {@render children()}
+  </div>
+</div>
+
+<style>
+.wrapper {
+  background-color: var(--auxie-deep-navy-900);
+  height: 100vh;
+}
+.setup_container {
+  height: 100%;
+  display: grid;
+  justify-content: center;
+  justify-items: center;
+  align-content: center;
+}
+
+</style>
